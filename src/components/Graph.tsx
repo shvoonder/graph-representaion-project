9957bc1eb06f7c0c88673f893eca9754944ec216
YarinKimhi <yarinkimhi2@gmail.com>
2020-02-09 17:08:54 +0200

    Update Graph.tsx

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { GraphView } from 'react-digraph';
 import { GraphConfig, NODE_KEY, sample } from 'src/graphConfig'
+import { node } from 'prop-types';
 
 export interface IGraphConfig {
   NodeTypes: INodeTypes,
@@ -59,7 +60,7 @@ interface IGraphProps {
 
 interface IGraphState {
   graph: IGraph,
-  selected: INode[]
+  selected: any
 }
 
 export class Graph extends React.Component<IGraphProps, IGraphState> {
@@ -114,4 +115,102 @@ export class Graph extends React.Component<IGraphProps, IGraphState> {
     })
   }
 
-}
+  onCreateNode = (x: number, y: number) => {
+    const graph = this.state.graph;
+    const type = "Custom";
+    const viewNode = {
+      id: Date.now(),
+      title: '',
+      type,
+      x,
+      y,
+    };
+    }
+
+    getNodeIndex(node: INode) {
+        return this.state.graph.nodes.findIndex(node => {
+          return node[NODE_KEY] === node[NODE_KEY];
+        });
+      }
+    
+    getEdgeIndex(edge: IEdge) {
+        return this.state.graph.edges.findIndex(edgeCheck => {
+            return(edgeCheck.source === edge.source && edgeCheck.target===edge.target 
+            );   
+        });
+      }
+
+   onUpdateNode (node: INode) {
+        const graph = this.state.graph;
+        const i = this.getNodeIndex(node);
+    
+        graph.nodes[i] = node;
+        this.setState({ graph });
+      };
+
+    onDeleteNode (node: INode, nodeId: string, nodeArr: INode[]){
+            const graph = this.state.graph;
+            const newEdges = graph.edges.filter((edge, i) => {
+              return (
+                edge.source !== node[NODE_KEY] && edge.target !== node[NODE_KEY]
+              );
+            });  
+      }
+    onSelectEdge(edge:IEdge){
+        const {selected}=this.state;
+        selected.push(edge)
+        this.setState({selected:selected});
+    }
+
+
+    onCreateEdge (sourceViewNode: INode, targetViewNode: INode){
+        const graph = this.state.graph;
+        const type ="Custom"
+        const viewEdge = {
+          source: sourceViewNode[NODE_KEY],
+          target: targetViewNode[NODE_KEY],
+          type,
+        };
+        if (viewEdge.source !== viewEdge.target) {
+          graph.edges = [...graph.edges, viewEdge];
+          this.setState({
+            graph,
+            selected: viewEdge,
+          });
+        }
+      };
+    
+      
+     onSwapEdge (sourceViewNode: INode,targetViewNode: INode,viewEdge: IEdge){
+        const graph = this.state.graph;
+        const i = this.getEdgeIndex(viewEdge);
+        const edge = JSON.parse(JSON.stringify(graph.edges[i]));
+
+        edge.source = sourceViewNode[NODE_KEY];
+        edge.target = targetViewNode[NODE_KEY];
+        graph.edges[i] = edge;
+        graph.edges = [...graph.edges];
+        this.setState({
+            graph,
+            selected: edge,
+        });
+    };
+    onDeleteEdge (viewEdge: IEdge, edges: IEdge[]) {
+        const graph = this.state.graph;
+
+        graph.edges = edges;
+        this.setState({
+            graph,
+            selected: null,
+        });
+    };
+
+
+
+
+
+    }  
+
+
+
+
